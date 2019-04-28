6ccb696d695e26d5af53b166467d10fafb1e9880
Ravi Dasari <Ravi.Dasari@netapp.com>
2019-04-28 18:49:12 -0400

    update custom scripts

--- custom/webui.bash
+++ custom/webui.bash
@@ -1,25 +1,59 @@
 #!/usr/bin/env bash
 
-echo 'Webui Custom Bash Profile -> Start'
+# echo 'Webui Custom Bash Profile -> Start'
 
 # general
 alias lsa='ls -lsah'
 alias ffind='find . -name'
 alias filefind='find . -name'
 alias myip='ifconfig | grep "inet " | grep -v 127.0.0.1'
+alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+# alias vnc='/Applications/TigerVNC\ Viewer\ 1.9.0.app/Contents/MacOS/TigerVNC\ Viewer'
+alias vcv-dev='/Applications/TigerVNC\ Viewer\ 1.9.0.app/Contents/MacOS/TigerVNC\ Viewer'
+alias max_user_watch='echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p'
 
 # git
 alias gitprune='git branch --merged | grep -v "\*" | grep -v master | grep -v dev | xargs -n 1 git branch -d'
 alias mybranch='git rev-parse --abbrev-ref HEAD'
 
 # webui
-alias webui='cd /gitviews/webui; pwd'
+alias webui='cd /gitviews/webui; pwd; npm start'
+alias webui-git='cd /gitviews/webui; pwd; gs'
 alias clearIdea='rm -rf /gitviews/webui/.idea; echo removed webui .idea'
-alias proxy='cd /gitviews/webui-proxy; pwd; ./b run'
+alias proxy='cd /gitviews/webui-proxy; pwd; node start'
 alias own_source='sudo chown -R rdasari /gitviews'
+alias webmount='sudo mount -t nfs -o resvport,rw 10.197.120.235:/gitviews /private/nfs'
+alias webumount='sudo umount /private/nfs'
 
 
 git config --system core.autocrlf input
+
+function update_theme(){
+  echo "this is used to keep ensure the hash doesn't get updated when updating to the latest theme"
+  git fetch --recurse-submodules
+  
+  echo
+  cd theme; pwd;
+  
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  
+  echo
+  echo "Stashing your local changes"
+  git stash
+  
+  echo 
+  echo "Checking out the $CURRENT_BRANCH"
+  git reset --hard origin/$CURRENT_BRANCH
+  
+  echo
+  echo "restoring your stash"
+  git stash pop
+  
+  cd ..
+  
+  echo "showing theme diff"
+  git diff theme 
+}
 
 function config_git {
   git config --global push.default current
@@ -93,6 +127,17 @@ function runvnc() {
   vncviewer -Maximize -AutoSelect=0 -LowColourLevel 2 -QualityLevel 0
 }
 
+function git-purge() {
+  echo "Pulling latest code..."
+  git pull
+  echo "Deleting local branches that were removed in remote..."
+  git fetch -p 
+  git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
+  echo "Remaining local branches:"
+  git branch -vv
+  echo "Done."
+}
+
 # only for webui
 function setupssh() {
     git remote set-url origin ssh://git@stash.nane.openenglab.netapp.com:7999/san/webui.git
@@ -100,7 +145,16 @@ function setupssh() {
     git remote set-url origin ssh://git@stash.nane.openenglab.netapp.com:7999/san/webui-theme.git
 }
 
-echo 'Webui Custom Bash Profile -> End'
+export SCM_GIT_SHOW_MINIMAL_INFO=true
+# export DISABLE_BASH_IT_THEME=true
+
+# parse_git_branch() {
+#   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+# }
+
+# export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+# echo 'Webui Custom Bash Profile -> End'
 
 
 
